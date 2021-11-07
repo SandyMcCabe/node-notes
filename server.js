47270cd0ce6cd660fb7436bd567f8f8560a2d267
Sandy McCabe <sandytech13@gmail.com>
2021-11-06 21:51:03 -0500

    pages load without data

--- server.js
+++ server.js
@@ -18,9 +18,6 @@ app.use(express.json());
 app.use(express.static('public'));
 
 
-// const apiRoutes = require('./routes/apiRoutes');
-// const htmlRoutes = require('./routes/htmlRoutes');
-
 function createNote(body, notesArray) {
     const note = body;
     notesArray.push(note);
@@ -34,11 +31,10 @@ function createNote(body, notesArray) {
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 app.get('/api/notes', (req, res) => {
     let results = notes;
-    console.log(req.query)
+    // console.log(req.query)
     res.json(results);
  });
 
-// app.use('/', apiRoutes);
 
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. 
 app.post('/api/notes', (req, res) => {
@@ -53,34 +49,24 @@ app.post('/api/notes', (req, res) => {
     // }
   });
 
+
+// GET /notes should return the notes.html file.
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+  }); 
+
+
+// GET * should return the index.html file.
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+}); 
+
+
+
 app.listen(PORT, () => {
 console.log(`API server now on port ${PORT}!`);
 });
 
-
-//   GIVEN a note-taking application
-// WHEN I open the Note Taker
-// THEN I am presented with a landing page with a link to a notes page
-// WHEN I click on the link to the notes page
-// THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
-// WHEN I enter a new note title and the note’s text
-// THEN a Save icon appears in the navigation at the top of the page
-// WHEN I click on the Save icon
-// THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
-// WHEN I click on an existing note in the list in the left-hand column
-// THEN that note appears in the right-hand column
-// WHEN I click on the Write icon in the navigation at the top of the page
-// THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
-
-
-
-// The following HTML routes should be created:
-
-// GET /notes should return the notes.html file.
-
-// GET * should return the index.html file.
-
-// The following API routes should be created:
 
 
 
