70efcaa2ef62248ae828169c08b28b7a33bb5b15
Sandy McCabe <sandytech13@gmail.com>
2021-11-07 19:47:48 -0600

    cleaned up code, added comments and a ReadMe

--- server.js
+++ server.js
@@ -3,12 +3,11 @@ const express = require('express');
 const app = express();
 
 // for the ids for the notes
-var uniqid = require('uniqid'); 
-
+var uniqid = require('uniqid');
 
 const PORT = process.env.PORT || 3001;
 
-const fs =require('fs');
+const fs = require('fs');
 const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
@@ -21,72 +20,40 @@ const { json } = require('express');
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 app.get('/api/notes', (req, res) => {
-    let results = notesArr
-    res.json(results);
- });
+  let results = notesArr
+  res.json(results);
+});
 
 
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. 
 app.post('/api/notes', (req, res) => {
-    // set id based on string returned from uniqid npi
-    let notes = notesArr
-    
-    req.body.id = uniqid();
-    notes.push(req.body)
+  // set id based on string returned from uniqid npi
+  let notes = notesArr
 
-    // if (!validateAnimal(req.body)) {
-    //   res.status(400).send('The animal is not properly formatted.');
-    // } else {
-      // notes = json.parse(notesArray)
-      // const note = createNote(req.body, notesArr);
+  req.body.id = uniqid();
+  notes.push(req.body)
 
-      fs.writeFileSync(
-                path.join(__dirname, './db/db.json'),
-                JSON.stringify(notes, null, 2)
-            );
-
-      res.json(notes);
-    // }
-  });
-
-  app.delete('/api/notes', (req, res) => {
-    let ID = req.body.id;
-    let notes = notesArr;
-    var indexNum
-
-    // let findIndex = (notesArr.indexOf(id: ID));
-    // console.log(findIndex);
-
-    for(var i = 0; i < notesArr.length; i++) {
-      if (notes.id === ID) {
-        indexNum = i;
-      }
-    };
-
-    notes.splice (indexNum, 1)
-
-    fs.writeFileSync(
-      path.join(__dirname, './db/db.json'),
-      JSON.stringify(notes, null, 2)
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify(notes, null, 2)
   );
 
-    res.json(notes);
- });
+  res.json(notes);
+});
 
 
 // GET /notes should return the notes.html file.
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-  }); 
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
 
 // GET * should return the index.html file.
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-}); 
-
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 
 app.listen(PORT, () => {
-console.log(`API server now on port ${PORT}!`);
+  console.log(`API server now on port ${PORT}!`);
 });
